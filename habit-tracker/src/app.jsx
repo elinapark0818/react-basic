4e37c935bf65d3939a623504ea62b8478e898ca9
Elina <elinapark0818@gmail.com>
2021-06-16 18:04:58 +0900

    master

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,8 +1,22 @@
+import { Component } from 'react';
 import './app.css';
 import Habits from "./habits";
 
-function App() {
-    return <Habits />;
+class App extends Component {
+    state = {
+        habits: [
+            { id : 1, name: 'Reading', count: 0},
+            { id : 2, name: 'Running', count: 0},
+            { id : 3, name: 'Coding', count: 0},
+        ],
+    };
+
+    handleCreate = habit => {
+        const { habits } = this.state;
+        this.setState({ 
+            habits: habits.concat({ id: this.id++, ...data}) });
+    };
+
 }
 
 export default App;
